595a61331ce5d6d2619990c677c21fb1eabdb9e3
aeroheart-c6 <aeroheart.c6@gmail.com>
2024-07-31 22:48:12 +0800

    update UpsertSubstations to referencing model

--- internal/repository/asset/switchboards.go
+++ internal/repository/asset/switchboards.go
@@ -9,58 +9,61 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func (i impl) UpsertSwitchboards(ctx context.Context, switchboards []model.Switchboard) error {
+func (i impl) UpsertSwitchboards(ctx context.Context, records []model.Switchboard) error {
 	var (
-		now    = time.Now().UTC()
-		writes = make([]mongo.WriteModel, 0, len(switchboards))
+		now      = time.Now().UTC()
+		ssWrites = make([]mongo.WriteModel, 0, len(records))
+		sbWrites = make([]mongo.WriteModel, 0, len(records))
 	)
 
-	for _, switchboard := range switchboards {
-		if switchboard.SubstationID.IsZero() && switchboard.SubstationAssetID == "" {
-			log.Println("oh??")
-			continue
+	for _, record := range records {
+		if record.ID.IsZero() {
+			record.ID = primitive.NewObjectID()
 		}
 
-		if switchboard.ID.IsZero() {
-			switchboard.ID = primitive.NewObjectID()
-		}
-
-		if switchboard.CreatedAt.IsZero() {
-			switchboard.CreatedAt = now
+		if record.CreatedAt.IsZero() {
+			record.CreatedAt = now
 		}
 
 		op := mongo.NewUpdateOneModel().
 			SetFilter(bson.D{
 				{Key: "$or", Value: bson.A{
-					bson.D{{Key: "_id", Value: switchboard.SubstationID}},
-					bson.D{{Key: "asset_id", Value: switchboard.SubstationAssetID}},
+					bson.D{{Key: "_id", Value: record.ID}},
+					bson.D{{Key: "asset_id", Value: record.AssetID}},
 				}},
 			}).
-			SetArrayFilters(options.ArrayFilters{
-				Filters: bson.A{
-					bson.D{
-						{Key: "$or", Value: bson.A{
-							bson.D{{Key: "elem._id", Value: switchboard.ID}},
-							bson.D{{Key: "elem.asset_id", Value: switchboard.AssetID}},
-						}},
-					},
-				},
-			}).
+			SetUpsert(true).
 			SetUpdate(bson.D{
+				{Key: "$setOnInsert", Value: bson.D{
+					{Key: "_id", Value: record.ID},
+					{Key: "panels", Value: bson.A{}},
+				}},
 				{Key: "$set", Value: bson.D{
-					{Key: "assets.switchboards.$[elem].asset_id", Value: switchboard.AssetID},
-					{Key: "assets.switchboards.$[elem].name", Value: switchboard.Name},
-					{Key: "assets.switchboards.$[elem].status", Value: switchboard.Status.String()},
-					{Key: "assets.switchboards.$[elem].network", Value: switchboard.Network.String()},
-					{Key: "assets.switchboards.$[elem].created_at", Value: switchboard.CreatedAt},
-					{Key: "assets.switchboards.$[elem].updated_at", Value: now},
+					{Key: "asset_id", Value: record.AssetID},
+					{Key: "name", Value: record.Name},
+					{Key: "status", Value: record.Status.String()},
+					{Key: "network", Value: record.Network.String()},
+					{Key: "created_at", Value: record.CreatedAt},
+					{Key: "updated_at", Value: now},
 				}},
 			})
 
-		writes = append(writes, op)
+		sbWrites = append(sbWrites, op)
+
+		op = mongo.NewUpdateOneModel().
+			SetFilter(bson.D{
+				{Key: "_id", Value: record.SubstationID},
+			}).
+			SetUpsert(true).
+			SetUpdate(bson.D{
+				{Key: "$addToSet", Value: bson.D{
+					{Key: "assets.switchboards", Value: record.ID},
+				}},
+			})
+
+		ssWrites = append(ssWrites, op)
 	}
 
 	ctxTimeout, ctxCancel := context.WithTimeout(ctx, 30*time.Second)
@@ -68,12 +71,23 @@ func (i impl) UpsertSwitchboards(ctx context.Context, switchboards []model.Switc
 
 	result, err := i.mongoConf.Client.
 		Database(i.mongoConf.DBName).
-		Collection("substations").
-		BulkWrite(ctxTimeout, writes)
+		Collection("switchboards").
+		BulkWrite(ctxTimeout, sbWrites)
 	if err != nil {
 		log.Println("query error", err)
 		return err
 	}
 	log.Printf("upserted records: %+v\n", result)
+
+	result, err = i.mongoConf.Client.
+		Database(i.mongoConf.DBName).
+		Collection("substations").
+		BulkWrite(ctxTimeout, ssWrites)
+	if err != nil {
+		log.Println("query error", err)
+		return err
+	}
+	log.Printf("upserted records: %+v\n", result)
+
 	return nil
 }
